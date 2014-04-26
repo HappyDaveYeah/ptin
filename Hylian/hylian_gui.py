64d2a936ebfca5b14a201079fbb07a09ace8e704
HappyDaveYeah <davidsantosp@gmail.com>
2014-04-26 19:33:33 +0200

    Search Input

--- Hylian/hylian_gui.py
+++ Hylian/hylian_gui.py
@@ -1,5 +1,5 @@
 import sys
-from PySide.QtGui import QApplication, QMainWindow, QDesktopWidget
+from PySide.QtGui import QApplication, QMainWindow, QDesktopWidget, QStatusBar, QTextEdit, QLineEdit
 
 
 class MainWindow(QMainWindow):
@@ -14,28 +14,6 @@ class MainWindow(QMainWindow):
         self.setGeometry(0, 0, 900, 600)
         self.center()
 
-    # def SetupComponents(self):
-    #     """ Function to setup status bar, central widget, menu bar
-    #     """
-    #     self.myStatusBar = QStatusBar()
-    #     self.setStatusBar(self.myStatusBar)
-    #     self.myStatusBar.showMessage('Ready', 10000)
-    #     self.textEdit = QTextEdit()
-    #     self.setCentralWidget(self.textEdit)
-    #     self.CreateActions()
-    #     self.CreateMenus()
-    #     self.fileMenu.addAction(self.newAction)
-    #     self.fileMenu.addSeparator()
-    #     self.fileMenu.addAction(self.exitAction)
-    #     self.editMenu.addAction(self.copyAction)
-    #     self.editMenu.addAction(self.pasteAction)
-    #     self.helpMenu.addAction(self.aboutAction)
-    #     self.CreateToolBar()
-    #     self.mainToolBar.addAction(self.newAction)
-    #     self.mainToolBar.addSeparator()
-    #     self.mainToolBar.addAction(self.copyAction)
-    #     self.mainToolBar.addAction(self.pasteAction)
-
     def center(self):
         """ Function to center the application
         """
@@ -44,10 +22,41 @@ class MainWindow(QMainWindow):
         qRect.moveCenter(centerPoint)
         self.move(qRect.topLeft())
 
+    def setupComponents(self):
+        """ Function to setup status bar, central widget, menu bar
+        """
+        self.myStatusBar = QStatusBar()
+        self.setStatusBar(self.myStatusBar)
+        self.myStatusBar.showMessage('Ready', 5000)
+        # self.textEdit = QTextEdit()
+        # self.setCentralWidget(self.textEdit)
+        self.createActions()
+        self.createToolBar()
+        search = QLineEdit()
+
+        self.mainToolBar.addWidget(search)
+        # self.mainToolBar.addAction(self.newAction)
+        # self.mainToolBar.addSeparator()
+        # self.mainToolBar.addAction(self.copyAction)
+        # self.mainToolBar.addAction(self.pasteAction)
+
+    def createActions(self):
+        """ Function to create actions for menus
+        """
+
+    def createToolBar(self):
+        """ Function to create and config the tool bar
+        """
+        self.mainToolBar = self.addToolBar('Main')
+        self.mainToolBar.setMovable(False)
+
+
+
 if __name__ == '__main__':
     try:
         myApp = QApplication(sys.argv)
         mainWindow = MainWindow()
+        mainWindow.setupComponents()
         mainWindow.show()
         myApp.exec_()
         sys.exit(0)

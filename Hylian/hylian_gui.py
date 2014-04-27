637745439bebf9464f055881f925bd246d7ac51b
HappyDaveYeah <davidsantosp@gmail.com>
2014-04-27 21:01:45 +0200

    Doing MenuBar

--- Hylian/hylian_gui.py
+++ Hylian/hylian_gui.py
@@ -1,5 +1,7 @@
 import sys
-from PySide.QtGui import QApplication, QMainWindow, QDesktopWidget, QStatusBar, QTextEdit, QLineEdit
+from PySide.QtCore import QSize
+from PySide.QtGui import QApplication, QMainWindow, QDesktopWidget, QStatusBar, QTextEdit, QLineEdit, QPushButton, \
+    QSizePolicy, QWidget, QFrame, QHBoxLayout, QStyleFactory, QVBoxLayout, QLabel, QToolButton, QIcon, QStyle, QPixmap
 
 
 class MainWindow(QMainWindow):
@@ -23,32 +25,82 @@ class MainWindow(QMainWindow):
         self.move(qRect.topLeft())
 
     def setupComponents(self):
-        """ Function to setup status bar, central widget, menu bar
+        """ Function to setup ...
         """
+
+        # Central Widget
+        centralWidget = QWidget()
+        self.setCentralWidget(centralWidget)
+
+        # Status Bar
         self.myStatusBar = QStatusBar()
         self.setStatusBar(self.myStatusBar)
         self.myStatusBar.showMessage('Ready', 5000)
-        # self.textEdit = QTextEdit()
-        # self.setCentralWidget(self.textEdit)
-        self.createActions()
-        self.createToolBar()
-        search = QLineEdit()
 
-        self.mainToolBar.addWidget(search)
-        # self.mainToolBar.addAction(self.newAction)
-        # self.mainToolBar.addSeparator()
-        # self.mainToolBar.addAction(self.copyAction)
-        # self.mainToolBar.addAction(self.pasteAction)
+        # Actions
+        self.createActions()
+
+        # Top QFrame
+        topFrame = QFrame()
+        topFrame.setFrameShape(QFrame.Box)
+
+        # Move Buttons
+        self.backBut = QToolButton()
+        self.backBut.setIcon(QIcon('../Resources/Icons/back.png'))
+        self.forwardBut = QToolButton()
+        self.forwardBut.setIcon(QIcon('../Resources/Icons/forward.png'))
+        moveLayout = QHBoxLayout()
+        moveLayout.addWidget(self.backBut)
+        moveLayout.addWidget(self.forwardBut)
+        moveLayout.setSpacing(0)
+
+        #
+        self.repBut = QToolButton()
+        self.repBut.setText("Repository")
+        self.configBut = QToolButton()
+        self.configBut.setText("Config Sys")
+        self.logBut = QToolButton()
+        self.logBut.setText("Logging")
+
+        #
+        self.search = QLineEdit()
+        self.search.setPlaceholderText("search")
+
+
+        menuLayout = QHBoxLayout()
+
+        menuLayout.addLayout(moveLayout)
+        menuLayout.addStretch(2.5)
+        menuLayout.addWidget(self.repBut)
+        menuLayout.addWidget(self.configBut)
+        menuLayout.addWidget(self.logBut)
+        menuLayout.addStretch(2)
+        menuLayout.addWidget(self.search)
+
+
+
+        self.testLabel = QLabel()
+        self.testLabel.setFrameShape(QFrame.Box)
+        self.testLabel.setText("Main")
+
+        bottomLayout = QVBoxLayout()
+        bottomLayout.addWidget(self.testLabel)
+
+        #topFrame.setLayout(menuLayout)
+        mainLayout = QVBoxLayout()
+        mainLayout.addLayout(menuLayout)
+        mainLayout.addLayout(bottomLayout)
+
+
+        centralWidget.setLayout(mainLayout)
+
+
+
 
     def createActions(self):
         """ Function to create actions for menus
         """
 
-    def createToolBar(self):
-        """ Function to create and config the tool bar
-        """
-        self.mainToolBar = self.addToolBar('Main')
-        self.mainToolBar.setMovable(False)
 
 
 

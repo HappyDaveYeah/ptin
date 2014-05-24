95cf6953e985e909ba0e87d861d951f912f6d1e3
HappyDaveYeah <davidsantosp@gmail.com>
2014-05-24 19:04:00 +0200

    dades de les apps en navi (fix)

--- Goron/cherryExample.py
+++ Goron/cherryExample.py
@@ -4,7 +4,7 @@ import json
 from subprocess import call
 import os
 
-apps = {"apps":[{"id":0,"enabled":1,"installed":1},{"id":1,"enabled":0,"installed":1},{"id":3,"enabled":0,"installed":1},{"id":5,"enabled":1,"installed":1}]}
+apps = {"apps":[{"id":0,"enabled":1},{"id":1,"enabled":0},{"id":3,"enabled":0},{"id":5,"enabled":1}]}
 
 
 # class user:
